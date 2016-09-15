215c8bbaf497173da9f9e243665543657de1fbbc
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2016-09-15 11:37:37 +0200

    Menu 2 new main options

---
+++ public/js/pages/pwDetail.js
@@ -0,0 +1,170 @@
+/*
+ * cUnitDetail.js
+ * Function for the page cUnitDetail.html
+*/
+var user = JSON.parse(aswCookies.getCookie('gdespa_user'));
+var api_key = aswCookies.getCookie('api_key')
+var lang = aswCookies.getCookie('gdespa_lang');
+
+
+var data = null;
+var vm = null;
+
+var cUnitDetailAPI = {
+    init: function () {
+        aswInit.initPage();
+        validator_languages(lang);
+        $('#user_name').text(user.name);
+        // make active menu option
+        $('#cUnitGeneral').attr('class', 'active');
+        // knockout management
+        vm = new cUnitDetailAPI.pageData();
+        ko.applyBindings(vm);
+        // buttons click events
+        $('#btnOk').click(cUnitDetailAPI.btnOk());
+        $('#btnExit').click(function (e) {
+            e.preventDefault();
+            window.open('cUnitGeneral.html', '_self');
+        })
+        // init lines table
+        cUnitLineAPI.init();
+        // init modal form
+        cUnitModalAPI.init();
+        // check if an id have been passed
+        var id = aswUtil.gup('id');
+        // if it is an update show lines
+        if (id != 0) {
+            $('#wid-id-1').show();
+        }
+        cUnitDetailAPI.getCUnit(id);
+    },
+    pageData: function () {
+        // knockout objects
+        var self = this;
+        self.id = ko.observable();
+        self.name = ko.observable();
+        self.reference = ko.observable();
+        self.description = ko.observable();
+        self.cost = ko.observable();
+        self.image = ko.observable();
+        // -- Modal related
+        self.lineId = ko.observable();
+        self.line = ko.observable();
+        self.quantity = ko.observable();
+        // item combo
+        self.optionsItems = ko.observableArray([]);
+        self.selectedItems = ko.observableArray([]);
+        self.sItem = ko.observable();
+        // unit combo
+        self.optionsUnits = ko.observableArray([]);
+        self.selectedUnits = ko.observableArray([]);
+        self.sUnit = ko.observable();
+    },
+    loadData: function (data) {
+        vm.id(data.id);
+        vm.name(data.name);
+        vm.reference(data.reference);
+        vm.description(data.description);
+        vm.cost(data.cost);
+        vm.image(data.image);
+    },
+    // Validates form (jquery validate) 
+    dataOk: function () {
+        $('#cUnitDetail-form').validate({
+            rules: {
+                txtName: { required: true },
+                txtReference: { required: true },
+                txtCost: {
+                    required: true,
+                    number: true
+                }
+            },
+            // Messages for form validation
+            messages: {
+            },
+            // Do not change code below
+            errorPlacement: function (error, element) {
+                error.insertAfter(element.parent());
+            }
+        });
+        return $('#cUnitDetail-form').valid();
+    },
+    // obtain a  cUnit group from the API
+    getCUnit: function (id) {
+        if (!id || (id == 0)) {
+            // new cUnit group
+            vm.id(0);
+            return;
+        }
+        var url = sprintf("%s/cunit/%s?api_key=%s", myconfig.apiUrl, id, api_key);
+        $.ajax({
+            type: "GET",
+            url: url,
+            contentType: "application/json",
+            success: function (data, status) {
+                cUnitDetailAPI.loadData(data[0]);
+                cUnitLineAPI.getCUnitLines(data[0].id);
+            },
+            error: function (err) {
+                aswNotif.errAjax(err);
+                if (err.status == 401) {
+                    window.open('login.html', '_self');
+                }
+            }
+        });
+    },
+    btnOk: function () {
+        var mf = function (e) {
+            // avoid default accion
+            e.preventDefault();
+            // validate form
+            if (!cUnitDetailAPI.dataOk()) return;
+            // dat for post or put
+            var data = {
+                id: vm.id(),
+                name: vm.name(),
+                reference: vm.reference(),
+                description: vm.description(),
+                image: vm.image(),
+                cost: vm.cost()
+            };
+            var url = "", type = "";
+            if (vm.id() == 0) {
+                // creating new record
+                type = "POST";
+                url = sprintf('%s/cunit?api_key=%s', myconfig.apiUrl, api_key);
+            } else {
+                // updating record
+                type = "PUT";
+                url = sprintf('%s/cunit/%s/?api_key=%s', myconfig.apiUrl, vm.id(), api_key);
+            }
+            $.ajax({
+                type: type,
+                url: url,
+                contentType: "application/json",
+                data: JSON.stringify(data),
+                success: function (data, status) {
+                    if (type == "POST") {
+                        vm.id(data.id);
+                         $('#wid-id-1').show();
+                    } else {
+                        var url = sprintf('cUnitGeneral.html?id=%s', data.id);
+                        window.open(url, '_self');
+                    }
+                },
+                error: function (err) {
+                    aswNotif.errAjax(err);
+                    if (err.status == 401) {
+                        window.open('login.html', '_self');
+                    }
+                }
+            });
+        }
+        return mf;
+    },
+};
+
+
+
+
+cUnitDetailAPI.init();
---
+++ public/js/pages/pwLine.js
@@ -0,0 +1,106 @@
+var cUnitLineAPI = {
+    init: function(){
+        // init tables
+        cUnitLineAPI.initCUnitLineTable();
+        // button handlers
+        $('#btnNewLine').click(cUnitLineAPI.newCUnitLine());
+        // avoid sending form 
+        $('#cUnitDetailLine-form').submit(function () {
+            return false;
+        });
+    },
+    initCUnitLineTable: function () {
+        var options = aswInit.initTableOptions('dt_cUnitLine');
+        options.data = data;
+        options.columns = [{
+            data: "line"
+        }, {
+                data: "item.name"
+            }, {
+                data: "unit.abb"
+            }, {
+                data: "quantity"
+            }, {
+                data: "id",
+                render: function (data, type, row) {
+                    var bt1 = "<button class='btn btn-circle btn-danger btn-lg' onclick='cUnitLineAPI.deleteCUnitLineMessage(" + data + ");' title='Eliminar registro'> <i class='fa fa-trash-o fa-fw'></i> </button>";
+                    var bt2 = "<button class='btn btn-circle btn-success btn-lg' data-toggle='modal' data-target='#cUnitModal' onclick='cUnitModalAPI.editLine(" + data + ");' title='Editar registro'> <i class='fa fa-edit fa-fw'></i> </button>";
+                    var html = "<div class='pull-right'>" + bt1 + " " + bt2 + "</div>";
+                    return html;
+                }
+            }];
+        $('#dt_cUnitLine').dataTable(options);
+    },
+    newCUnitLine: function(){
+        var mf = function(e){
+            // show modal form
+            e.preventDefault();
+            cUnitModalAPI.newLine();
+        };
+        return mf;
+    },
+    deleteCUnitLineMessage: function (id) {
+        var url = sprintf("%s/cunit_line/%s/?api_key=%s", myconfig.apiUrl, id, api_key);
+        $.ajax({
+            type: "GET",
+            url: url,
+            contentType: "application/json",
+            success: function (data, status) {
+                var name = data[0].item.name;
+                var fn = sprintf('cUnitLineAPI.deleteCUnitLine(%s);', id);
+                aswNotif.deleteRecordQuestion(name, fn);
+            },
+            error: function (err) {
+                aswNotif.errAjax(err);
+                if (err.status == 401) {
+                    window.open('login.html', '_self');
+                }
+            }
+        });
+    },
+    deleteCUnitLine: function (id) {
+        var url = sprintf("%s/cunit_line/%s/?api_key=%s", myconfig.apiUrl, id, api_key);
+        var data = {
+            id: id
+        };
+        $.ajax({
+            type: "DELETE",
+            url: url,
+            contentType: "application/json",
+            data: JSON.stringify(data),
+            success: function (data, status) {
+                cUnitLineAPI.getCUnitLines(vm.id());
+            },
+            error: function (err) {
+                aswNotif.errAjax(err);
+                if (err.status == 401) {
+                    window.open('login.html', '_self');
+                }
+            }
+        });
+    },
+    getCUnitLines: function (id) {
+        var url = sprintf("%s/cunit_line/cunit/%s/?api_key=%s", myconfig.apiUrl, id, api_key);
+        $.ajax({
+            type: "GET",
+            url: url,
+            contentType: "application/json",
+            success: function (data, status) {
+                cUnitLineAPI.loadCUnitLinesTable(data);
+            },
+            error: function (err) {
+                aswNotif.errAjax(err);
+                if (err.status == 401) {
+                    window.open('login.html', '_self');
+                }
+            }
+        });
+    },
+    loadCUnitLinesTable: function (data) {
+        var dt = $('#dt_cUnitLine').dataTable();
+        dt.fnClearTable();
+        if (data.length && data.length > 0) dt.fnAddData(data);
+        dt.fnDraw();
+        $("#tb_cUnitLine").show();
+    }
+};
