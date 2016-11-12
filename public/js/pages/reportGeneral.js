fed01e383e8e638ba9adc65d8bd5689625bbeb9e
Rafael Garcia <dev.ragare@gmail.com>
2016-11-12 13:58:17 +0100

    ProvCopy

--- public/js/pages/reportGeneral.js
+++ public/js/pages/reportGeneral.js
@@ -27,6 +27,14 @@ var reportGeneralAPI = {
         $('#closureDetail-form').submit(function() {
             return false;
         });
+        // combos
+        $('#cmbPwStatus').select2(select2_languages[lang]);
+        reportGeneralAPI.loadPwStatus();
+        $('#btnPrintStatus').click(reportGeneralAPI.btnPrintStatus());
+        // avoid sending form 
+        $('#pwStatusDetail-form').submit(function() {
+            return false;
+        });
     },
     pageData: function() {
         var self = this;
@@ -34,6 +42,10 @@ var reportGeneralAPI = {
         self.optionsClosures = ko.observableArray([]);
         self.selectedClosures = ko.observableArray([]);
         self.sClosure = ko.observable();
+        // pw status combo
+        self.optionsPwStatus = ko.observableArray([]);
+        self.selectedPwStatus = ko.observableArray([]);
+        self.sPwStatus = ko.observable();
     },
     loadClosures: function(id) {
         $.ajax({
@@ -81,7 +93,55 @@ var reportGeneralAPI = {
             });
         }
         return mf;
+    },
+    loadPwStatus: function(id) {
+        $.ajax({
+            type: "GET",
+            url: sprintf('%s/pw/report_pw/?api_key=%s', myconfig.apiUrl, api_key),
+            dataType: "json",
+            contentType: "application/json",
+            success: function(data, status) {
+                var options = [{ id: 0, name: " " }].concat(data);
+                vm.optionsPwStatus(options);
+                $("#cmbPwStatus").val([id]).trigger('change');
+            },
+            error: function(err) {
+                aswNotif.errAjax(err);
+                if (err.status == 401) {
+                    window.open('index.html', '_self');
+                }
+            }
+        });
+    },
+    btnPrintPwStatus: function() {
+        var mf = function(e) {
+            // avoid default accion
+            e.preventDefault();
+            var url = "", type = "";
+
+            // fecth report data
+            type = "GET";
+            url = sprintf('%s/report/closure/%s/?api_key=%s', myconfig.apiUrl, vm.sPwStatus(), api_key);
+            $.ajax({
+                type: type,
+                url: url,
+                contentType: "application/json",
+                data: JSON.stringify(data),
+                success: function(data, status) {
+                    // process report data
+                    aswReport.reportPDF(data, 'S12MqKVWl');
+                },
+                error: function(err) {
+                    aswNotif.errAjax(err);
+                    if (err.status == 401) {
+                        window.open('index.html', '_self');
+                    }
+                }
+            });
+        }
+        return mf;
     }
+
 
 };
 
