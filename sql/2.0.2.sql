9aeb8c5b69ff23af4bf01ce705d3b4e1c3f2691b
Rafa <rafa@myariadna.com>
2017-02-16 13:06:02 +0100

    VRS 2.0.2

--- sql/2.0.2.sql
+++ sql/2.0.2.sql
@@ -1,3 +1,9 @@
 ALTER TABLE `wo_line`   
   ADD COLUMN `chapterId` INT(11) NULL AFTER `quantity`,
   ADD CONSTRAINT `ref_wol_chapter` FOREIGN KEY (`chapterId`) REFERENCES `chapter`(`chapterId`);
+
+UPDATE wo_line AS wol, wo, chapter AS ch
+  SET wol.chapterId = ch.chapterId
+  WHERE wo.woId = wol.woId
+  AND ch.pwId = wo.pwId
+  AND wol.chapterId IS NULL;  
