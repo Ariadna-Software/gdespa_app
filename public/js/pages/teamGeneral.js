554cd4610dca923ee217155ed4a05237e104ab07
Rafael Garcia <dev.ragare@gmail.com>
2017-06-01 00:45:29 +0200

    Brigadas (2)

--- public/js/pages/teamGeneral.js
+++ public/js/pages/teamGeneral.js
@@ -1,90 +1,80 @@
 /*
- * itemInGeneral.js
- * Function for the page itemInGeneral.html
+ * teamGeneral.js
+ * Function for the page teamGeneral.html
 */
 var user = JSON.parse(aswCookies.getCookie('gdespa_user'));
 var api_key = aswCookies.getCookie('api_key')
 
 var data = null;
 
-var itemInGeneralAPI = {
+var teamGeneralAPI = {
     init: function () {
         $('#user_name').text(user.name);
         aswInit.initPerm(user);
         // make active menu option
-        $('#itemInGeneral').attr('class', 'active');
-        itemInGeneralAPI.inititemInTable();
+        $('#teamGeneral').attr('class', 'active');
+        teamGeneralAPI.initTeamTable();
         // avoid sending form 
-        $('#itemInGeneral-form').submit(function () {
+        $('#teamGeneral-form').submit(function () {
             return false;
         });
         // buttons click events 
-        $('#btnNew').click(itemInGeneralAPI.newitemIn());
-        $('#btnSearch').click(itemInGeneralAPI.searchitemIn());
+        $('#btnNew').click(teamGeneralAPI.newTeam());
+        $('#btnSearch').click(teamGeneralAPI.searchTeam());
         // check if there's an id
         var id = aswUtil.gup('id');
         if (id) {
-            itemInGeneralAPI.getitemIn(id);
+            teamGeneralAPI.getTeam(id);
         } else {
-            itemInGeneralAPI.getitemIn('');
+            teamGeneralAPI.getTeam('');
         }
     },
     // initializes the table
-    inititemInTable: function () {
-        var options = aswInit.initTableOptions('dt_itemIn');
+    initTeamTable: function () {
+        var options = aswInit.initTableOptions('dt_team');
         options.data = data;
         options.columns = [{
-            data: "store.name"
+            data: "name"
         }, {
-            data: "dateIn",
+            data: "workerInChargeName",
+        }, {
+            data: "teamId",
             render: function (data, type, row) {
-                // LANG: var html = moment(data).format(i18n.t('util.date_format'));
-                var html = moment(data).format('DD/MM/YYYY');
-                html = "<div class='asw-center'>" + html + "</div>";
-                return html;
-            }
-        }, {
-            data: "worker.name",
-        }, {
-            data: "comments"
-        }, {
-            data: "id",
-            render: function (data, type, row) {
-                var bt1 = "<button class='btn btn-circle btn-danger btn-lg' onclick='itemInGeneralAPI.deleteitemInMessage(" + data + ");' title='Eliminar registro'> <i class='fa fa-trash-o fa-fw'></i> </button>";
-                var bt2 = "<button class='btn btn-circle btn-success btn-lg' onclick='itemInGeneralAPI.edititemIn(" + data + ");' title='Editar registro'> <i class='fa fa-edit fa-fw'></i> </button>";
+                var bt1 = "<button class='btn btn-circle btn-danger btn-lg' onclick='teamGeneralAPI.deleteTeamMessage(" + data + ");' title='Eliminar registro'> <i class='fa fa-trash-o fa-fw'></i> </button>";
+                var bt2 = "<button class='btn btn-circle btn-success btn-lg' onclick='teamGeneralAPI.editTeam(" + data + ");' title='Editar registro'> <i class='fa fa-edit fa-fw'></i> </button>";
                 var html = "<div class='pull-right'>" + bt1 + " " + bt2 + "</div>";
                 return html;
             }
         }];
-        $('#dt_itemIn').dataTable(options);
+        $('#dt_team').dataTable(options);
     },
-    searchitemIn: function () {
+    searchTeam: function () {
         var mf = function () {
             // obtain strin to search 
             var search = $('#txtSearch').val();
-            itemInGeneralAPI.getitemIns(search);
+            teamGeneralAPI.getTeams(search);
         };
         return mf;
     },
-    newitemIn: function () {
+    newTeam: function () {
         // Its an event handler, return function
         var mf = function () {
-            window.open(sprintf('itemInDetail.html?id=%s', 0), '_self');
+            window.open(sprintf('teamDetail.html?id=%s', 0), '_self');
         }
         return mf;
     },
-    edititemIn: function (id) {
-        window.open(sprintf('itemInDetail.html?id=%s', id), '_self');
+    editTeam: function (id) {
+        window.open(sprintf('teamDetail.html?id=%s', id), '_self');
     },
-    deleteitemInMessage: function (id) {
-        var url = sprintf("%s/item_in/%s/?api_key=%s", myconfig.apiUrl, id, api_key);
+    deleteTeamMessage: function (id) {
+        var url = sprintf("%s/team/%s/?api_key=%s", myconfig.apiUrl, id, api_key);
         $.ajax({
             type: "GET",
             url: url,
             contentType: "application/json",
             success: function (data, status) {
-                var name = data[0].store.name + " [" + moment(data[0].dateIn).format(i18n.t('util.date_format')) + "]";
-                var fn = sprintf('itemInGeneralAPI.deleteitemIn(%s);', id);
+                var name = data[0].name + " [" + moment(data[0].dateIn).format(i18n.t('util.date_format')) + "]";
+                var fn = sprintf('teamGeneralAPI.deleteTeam(%s);', id);
                 aswNotif.deleteRecordQuestion(name, fn);
             },
             error: function (err) {
@@ -95,8 +85,8 @@ var itemInGeneralAPI = {
             }
         });
     },
-    deleteitemIn: function (id) {
-        var url = sprintf("%s/item_in/%s/?api_key=%s", myconfig.apiUrl, id, api_key);
+    deleteTeam: function (id) {
+        var url = sprintf("%s/team/%s/?api_key=%s", myconfig.apiUrl, id, api_key);
         var data = {
             id: id
         };
@@ -106,7 +96,7 @@ var itemInGeneralAPI = {
             contentType: "application/json",
             data: JSON.stringify(data),
             success: function (data, status) {
-                itemInGeneralAPI.getitemIns('');
+                teamGeneralAPI.getTeams('');
             },
             error: function (err) {
                 aswNotif.errAjax(err);
@@ -117,30 +107,14 @@ var itemInGeneralAPI = {
         });
     },
     // obtain user groups from the API
-    getitemIns: function (name) {
-        var url = sprintf("%s/item_in?api_key=%s&name=%s", myconfig.apiUrl, api_key, name);
+    getTeams: function (name) {
+        var url = sprintf("%s/team?api_key=%s&name=%s", myconfig.apiUrl, api_key, name);
         $.ajax({
             type: "GET",
             url: url,
             contentType: "application/json",
             success: function (data, status) {
-                var data2 = [];
-                if (!user.seeNotOwner) {
-                    data.forEach(function (d) {
-                        if (d.worker.id == user.worker.id) {
-                            data2.push(d);
-                        } else {
-                            if (user.seeZone) {
-                                if (d.zoneId == user.zoneId) {
-                                    data2.push(d);
-                                }
-                            }
-                        }
-                    });
-                } else {
-                    data2 = data;
-                }
-                itemInGeneralAPI.loaditemInsTable(data2);
+                teamGeneralAPI.loadTeamsTable(data);
             },
             error: function (err) {
                 aswNotif.errAjax(err);
@@ -151,26 +125,14 @@ var itemInGeneralAPI = {
         });
     },
     // obtain user groups from the API
-    getitemIn: function (id) {
-        var url = sprintf("%s/item_in/%s/?api_key=%s", myconfig.apiUrl, id, api_key);
+    getTeam: function (id) {
+        var url = sprintf("%s/team/%s/?api_key=%s", myconfig.apiUrl, id, api_key);
         $.ajax({
             type: "GET",
             url: url,
             contentType: "application/json",
             success: function (data, status) {
-                var data2 = [];
-                if (!user.seeNotOwner) {
-                    if (user.worker) {
-                        data.forEach(function (d) {
-                            if (d.worker.id == user.worker.id) {
-                                data2.push(d);
-                            }
-                        });
-                    }
-                } else {
-                    data2 = data;
-                }
-                itemInGeneralAPI.loaditemInsTable(data2);
+                teamGeneralAPI.loadTeamsTable(data);
             },
             error: function (err) {
                 aswNotif.errAjax(err);
@@ -180,13 +142,13 @@ var itemInGeneralAPI = {
             }
         });
     },
-    loaditemInsTable: function (data) {
-        var dt = $('#dt_itemIn').dataTable();
+    loadTeamsTable: function (data) {
+        var dt = $('#dt_team').dataTable();
         dt.fnClearTable();
         if (data.length && data.length > 0) dt.fnAddData(data);
         dt.fnDraw();
-        $("#tb_itemIn").show();
+        $("#tb_team").show();
     }
 };
 
-itemInGeneralAPI.init();
+teamGeneralAPI.init();
