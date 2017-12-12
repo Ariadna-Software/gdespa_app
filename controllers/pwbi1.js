c92933e31be4fad5847103487b3623be8801ad43
Rafael Garcia <rafa@myariadna.com>
2017-12-12 18:03:43 +0100

    Unificación pwbi

--- controllers/pwbi1.js
+++ controllers/pwbi1.js
@@ -49,4 +49,109 @@ router.get('/doctypes', function (req, res) {
     });
 });
 
+router.get('/chapters', function (req, res) {
+    pwbi1Db.getChapters((err, regs) => {
+        if (err) return res.status(500).send(err.message);
+        res.json(regs);
+    });
+});
+
+router.get('/pwlines', function (req, res) {
+    pwbi1Db.getPwLines((err, regs) => {
+        if (err) return res.status(500).send(err.message);
+        res.json(regs);
+    });
+});
+
+router.get('/cunits', function (req, res) {
+    pwbi1Db.getCUnits((err, regs) => {
+        if (err) return res.status(500).send(err.message);
+        res.json(regs);
+    });
+});
+
+router.get('/cunitlines', function (req, res) {
+    pwbi1Db.getCunitLines((err, regs) => {
+        if (err) return res.status(500).send(err.message);
+        res.json(regs);
+    });
+});
+
+router.get('/items', function (req, res) {
+    pwbi1Db.getItems((err, regs) => {
+        if (err) return res.status(500).send(err.message);
+        res.json(regs);
+    });
+});
+
+router.get('/wos', function (req, res) {
+    pwbi1Db.getWos((err, regs) => {
+        if (err) return res.status(500).send(err.message);
+        res.json(regs);
+    });
+});
+
+router.get('/teams', function (req, res) {
+    pwbi1Db.getTeams((err, regs) => {
+        if (err) return res.status(500).send(err.message);
+        res.json(regs);
+    });
+});
+
+router.get('/wolines', function (req, res) {
+    pwbi1Db.getWoLines((err, regs) => {
+        if (err) return res.status(500).send(err.message);
+        res.json(regs);
+    });
+});
+
+router.get('/invoices', function (req, res) {
+    pwbi1Db.getInvoices((err, regs) => {
+        if (err) return res.status(500).send(err.message);
+        res.json(regs);
+    });
+});
+
+router.get('/workedhours', function (req, res) {
+    pwbi1Db.getWorkedHours((err, regs) => {
+        if (err) return res.status(500).send(err.message);
+        res.json(regs);
+    });
+});
+
+router.get('/teamlines', function (req, res) {
+    pwbi1Db.getTeamLines((err, regs) => {
+        if (err) return res.status(500).send(err.message);
+        res.json(regs);
+    });
+});
+
+router.get('/lumhours', function (req, res) {
+    pwbi1Db.getLumHours((err, regs) => {
+        if (err) return res.status(500).send(err.message);
+        res.json(regs);
+    });
+});
+
+router.get('/contahours', function (req, res) {
+    pwbi1Db.getContaHours((err, regs) => {
+        if (err) return res.status(500).send(err.message);
+        res.json(regs);
+    });
+});
+
+router.get('/woitems', function (req, res) {
+    pwbi1Db.getWoItems((err, regs) => {
+        if (err) return res.status(500).send(err.message);
+        res.json(regs);
+    });
+});
+
+router.get('/pwitems', function (req, res) {
+    pwbi1Db.getPwItems((err, regs) => {
+        if (err) return res.status(500).send(err.message);
+        res.json(regs);
+    });
+});
+
 module.exports = router;
